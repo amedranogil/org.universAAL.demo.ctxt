5bddfa55c5e19a8929022ba1b1a56ecabd36ae4d
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2017-10-27 09:11:17 +0200

    Generation of demo using Archetype: org.universAAL.samples module.generic-archetype 3.4.1-SNAPSHOT http://depot.universaal.org/nexus/content/repositories/snapshots/

---
+++ src/main/java/org/accrossing/universAAL/demo/ProjectActivator.java
@@ -0,0 +1,39 @@
+package org.accrossing.universAAL.demo;
+
+import org.universAAL.middleware.container.ModuleActivator;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+
+
+public class ProjectActivator implements ModuleActivator {
+
+	static ModuleContext context;
+
+	static ContextPublisher myPublisher;
+
+	static ContextSubscriber mySubscriber;
+	
+	private Thread worker;
+
+	public void start(ModuleContext ctxt) throws Exception {
+		context = ctxt;
+		LogUtils.logDebug(context, getClass(), "start", "Starting.");
+		/*
+		 * uAAL stuff
+		 */
+
+		
+		LogUtils.logDebug(context, getClass(), "start", "Started.");
+	}
+
+	public void stop(ModuleContext ctxt) throws Exception {
+		LogUtils.logDebug(context, getClass(), "stop", "Stopping.");
+		/*
+		 * close uAAL stuff
+		 */
+
+		LogUtils.logDebug(context, getClass(), "stop", "Stopped.");
+
+	}
+
+}
---
+++ src/test/java/org/accrossing/universAAL/demo/MyTest.java
@@ -0,0 +1,10 @@
+package org.accrossing.universAAL.demo;
+
+import org.universAAL.middleware.bus.junit.OntTestCase;
+
+public class MyTest extends OntTestCase {
+
+    public void test() {
+    	generateOntFiles4MyProy();
+    }
+}
